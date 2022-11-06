43742755319a8b4f33543f33b15bda044aaa8f63
RICARDO MENDONCA <ricardo-mendonca@users.noreply.github.com>
2022-11-06 19:14:17 -0300

    identação documento

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
-    return (
-        <Routes>
-<Route path="/pagina-inicial" element={<p>pagina inicial</p>} />
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>pagina inicial</p>} />
 
-<Route path="*" element={<Navigate to="/pagina-inicial" />} />
-        </Routes>
-    )
-
-}
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
