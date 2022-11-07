33d1231d71acd7600b02325d24c5d5ea5cfa63f4
RICARDO MENDONCA <ricardo-mendonca@users.noreply.github.com>
2022-11-07 19:40:47 -0300

    Atualização do Menu Lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -45,7 +45,7 @@ export const MenuLateral: React.FC<IAppMenuLateralProps> = ({ children }) => {
   return (
     <>
       <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
-        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+        <Box width={theme.spacing(27)} height="100%" display="flex" flexDirection="column">
 
           <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
             <Avatar
