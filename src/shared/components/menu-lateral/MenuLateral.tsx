b265c911cf1da66412663416d4b798393e373073
RICARDO MENDONCA <ricardo-mendonca@users.noreply.github.com>
2022-11-07 15:06:26 -0300

    feat: adicionado responsividade no menu lateral

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,68 @@
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import { useDrawerContext } from "../../contexts";
+
+interface IAppMenuLateralProps {
+  children: React.ReactNode;
+}
+
+export const MenuLateral: React.FC<IAppMenuLateralProps> = ({ children }) => {
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const { isDrawerOpen, toggleDrawerOpen} = useDrawerContext();
+
+  return (
+    <>
+      <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
+        <Box
+          width={theme.spacing(27)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://images.vexels.com/media/users/3/145908/raw/52eabf633ca6414e60a7677b0b917d92-criador-de-avatar-masculino.jpg"
+            />
+          </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+
+      <Box height="100vh" marginLeft={ smDown ? 0 : theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
