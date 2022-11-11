8726a71b9bcbcc0718845e0c20a2c383bfba55f0
RICARDO MENDONCA <ricardo-mendonca@users.noreply.github.com>
2022-11-11 19:45:27 -0300

    Atualizado barra de ferramentas

---
+++ src/shared/components/ferramentas-de-detalhe/FerramentaDeDetalhe.tsx
@@ -0,0 +1,108 @@
+import { TrendingUp } from "@mui/icons-material";
+import { Box, Button, Divider, Icon, Paper, useTheme } from "@mui/material";
+
+interface IFerramentasDeDetalheProps {
+  textoBotaoNovo?: string;
+
+  mostrarBotaoNovo?: boolean;
+  mostrarBotaoVoltar?: boolean;
+  mostrarBotaoApagar?: boolean;
+  mostrarBotaoSalvar?: boolean;
+  mostrarBotaoSalvarEFechar?: boolean;
+
+  aoClicarEmNovo?: () => void;
+  aoClicarEmVoltar?: () => void;
+  aoClicarEmApagar?: () => void;
+  aoClicarEmSalvar?: () => void;
+  aoClicarEmSalvarEFechar?: () => void;
+}
+
+export const FerramentasDeDetalhe: React.FC<IFerramentasDeDetalheProps> = ({
+  textoBotaoNovo = "Novo",
+
+  mostrarBotaoNovo = true,
+  mostrarBotaoVoltar = true,
+  mostrarBotaoApagar = true,
+  mostrarBotaoSalvar = true,
+  mostrarBotaoSalvarEFechar = false,
+
+  aoClicarEmNovo,
+  aoClicarEmVoltar,
+  aoClicarEmApagar,
+  aoClicarEmSalvar,
+  aoClicarEmSalvarEFechar,
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      gap={1}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      alignItems="center"
+      height={theme.spacing(5)}
+      component={Paper}
+    >
+      {mostrarBotaoSalvar && (
+        <Button
+          color="primary"
+          disableElevation
+          variant="contained"
+          onClick={aoClicarEmSalvar}
+          startIcon={<Icon>save</Icon>}
+        >
+          Salvar
+        </Button>
+      )}
+
+      {mostrarBotaoSalvarEFechar && (
+        <Button
+          color="primary"
+          disableElevation
+          variant="outlined"
+          onClick={aoClicarEmSalvarEFechar}
+          startIcon={<Icon>save</Icon>}
+        >
+          Salvar e voltar
+        </Button>
+      )}
+      {mostrarBotaoApagar && (
+        <Button
+          color="primary"
+          disableElevation
+          variant="outlined"
+          onClick={aoClicarEmApagar}
+          startIcon={<Icon>delete</Icon>}
+        >
+          Apagar
+        </Button>
+      )}
+
+      {mostrarBotaoNovo && (
+        <Button
+          color="primary"
+          disableElevation
+          variant="outlined"
+          onClick={aoClicarEmNovo}
+          startIcon={<Icon>add</Icon>}
+        >{textoBotaoNovo}
+        </Button>
+      )}
+      <Divider variant="middle" orientation="vertical" />
+
+      {mostrarBotaoVoltar && (
+        <Button
+          color="primary"
+          disableElevation
+          variant="outlined"
+          onClick={aoClicarEmVoltar}
+          startIcon={<Icon>arrow_back</Icon>}
+        >
+          Voltar
+        </Button>
+      )}
+    </Box>
+  );
+};
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,2 +1,3 @@
 export * from './menu-lateral/MenuLateral';
 export * from './Ferramentas-da-listagem/FerramentasDaListagem';
+export * from './ferramentas-de-detalhe/FerramentaDeDetalhe';
