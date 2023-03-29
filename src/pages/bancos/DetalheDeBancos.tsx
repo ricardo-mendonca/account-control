19c7b56ef9441695558c1da315ca3b4f25c38116
ricardo-mendonca <ricardo-mendonca@live.com>
2023-03-28 21:42:38 -0300

    Atualização da tela de bancos CRUD

--- src/pages/bancos/DetalheDeBancos.tsx
+++ src/pages/bancos/DetalheDeBancos.tsx
@@ -1,7 +1,196 @@
+/* eslint-disable eqeqeq */
+import { Box, Grid, LinearProgress, Paper, Switch, Typography } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import * as yup from 'yup';
 
+import { BancosService } from "../../shared/services/api/Bancos/BancosService";
+import { VTextField, VForm, useVForm, IVFormErrors } from "../../shared/forms";
+import { FerramentasDeDetalhe } from "../../shared/components";
+import { LayoutBaseDePagina } from "../../shared/layouts";
+
+interface IFormData {
+    nome: string;
+    ativo: string;
+}
+
+const formValidationSchema: yup.SchemaOf<IFormData> = yup.object().shape({
+    nome: yup.string().required().min(3).max(30),
+    ativo: yup.string().required()
+});
 
 export const DetalheDeBanco = () => {
-    return(
-        <div>listagem de banco</div>
-    )
-}
+    const { formRef, save, saveAndClose, isSaveAndClose } = useVForm();
+    const { id = "nova" } = useParams<"id">();
+    const navigate = useNavigate();
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [nome, setNome] = useState("");
+
+
+    //Switch
+    const [checked, setChecked] = useState(true);
+    const handleChangeSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+    };
+
+    useEffect(() => {
+        if (id !== "nova") {
+            setIsLoading(true);
+
+            BancosService.getById(Number(id)).then((result) => {
+                setIsLoading(false);
+
+                if (result instanceof Error) {
+                    alert(" OPS!! algo deu errado \n" + result.message);
+                    navigate("/bancos");
+                } else {
+                    setNome(result.nome);
+
+                    setChecked(result.ativo == '1' ? true : false);
+                    formRef.current?.setData(result);
+                }
+            });
+        } else {
+            formRef.current?.setData({
+                nome: '',
+                ativo: '',
+            })
+        }
+    }, [formRef, id, navigate]);
+
+    const handleSave = (dados: IFormData) => {
+
+        dados.ativo = (checked === true ? '1' : '0');
+
+        formValidationSchema
+            .validate(dados, { abortEarly: false })
+            .then((dadosValidados) => {
+                setIsLoading(true);
+
+                if (id === "nova") {
+                    BancosService.create(dadosValidados).then((result) => {
+                        setIsLoading(false);
+                        if (result instanceof Error) {
+                            alert("Ops!! algo deu ruim! \n" + result.message);
+                        } else {
+                            if (isSaveAndClose()) {
+                                navigate('/bancos');
+                            } else {
+                                navigate(`/bancos/${result}`);
+                            }
+                        }
+                    });
+                } else {
+                    setIsLoading(true);
+                    BancosService.updateById(Number(id), { id: Number(id), ...dadosValidados }).then((result) => {
+                        setIsLoading(false);
+                        if (result instanceof Error) {
+                            alert("Ops!! algo deu ruim! \n" + result.message);
+                        } else {
+                            if (isSaveAndClose()) {
+                                navigate('/bancos');
+                            }
+                        }
+                    });
+                }
+
+
+            })
+            .catch((errors: yup.ValidationError) => {
+                const validationErrors: IVFormErrors = {};
+
+                errors.inner.forEach(error => {
+                    if (!error.path) return;
+                    validationErrors[error.path] = error.message;
+                });
+                formRef.current?.setErrors(validationErrors);
+            });
+    };
+
+    const handleDelete = (id: number) => {
+        if (
+            window.confirm(
+                "Mano, tem certeza que quer apagar o registro " + id + " ?"
+            )
+        ) {
+            BancosService.deleteById(id).then((result) => {
+                if (result instanceof Error) {
+                    alert(result.message);
+                } else {
+                    alert("Registro apagado com sucesso!");
+                    navigate("/bancos");
+                }
+            });
+        }
+    };
+
+
+
+    return (
+        <LayoutBaseDePagina
+            titulo={id === "novo" ? "Novo Banco" : nome}
+            barraDeFerramentas={
+                <FerramentasDeDetalhe
+                    textoBotaoNovo="Novo"
+                    mostrarBotaoSalvarEFechar
+                    mostrarBotaoNovo={id !== "novo"}
+                    mostrarBotaoApagar={id !== "novo"}
+
+                    aoClicarEmSalvar={save}
+                    aoClicarEmSalvarEFechar={saveAndClose}
+                    aoClicarEmApagar={() => handleDelete(Number(id))}
+                    aoClicarEmNovo={() => navigate("/bancos/nova")}
+                    aoClicarEmVoltar={() => navigate("/bancos")}
+                />
+            }
+        >
+            <VForm ref={formRef} onSubmit={handleSave}>
+                <Box
+                    margin={1}
+                    display="flex"
+                    flexDirection="column"
+                    component={Paper}
+                    variant="outlined"
+                >
+                    <Grid container direction="column" padding={2} spacing={2}>
+
+                        {isLoading && (
+                            <Grid item>
+                                <LinearProgress variant="indeterminate"></LinearProgress>
+                            </Grid>
+                        )}
+
+                        <Grid item>
+                            <Typography variant="h6">Geral</Typography>
+                        </Grid>
+
+                        <Grid container item direction="row" spacing={2}>
+                            <Grid item xs={12} sm={8} md={6} lg={6} xl={6}>
+                                <VTextField
+                                    fullWidth
+                                    name="nome"
+                                    disabled={isLoading}
+                                    label="Nome"
+                                    onChange={e => setNome(e.target.value)}
+                                />
+                            </Grid>
+                        </Grid>
+
+                        <Grid container item direction="row">
+                            <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                                <label>Conta Ativa ? </label>
+                                <Switch
+                                    checked={checked}
+                                    onChange={handleChangeSwitch}
+                                    inputProps={{ 'aria-label': 'controlled' }}
+                                />
+                            </Grid>
+                        </Grid>
+
+                    </Grid>
+                </Box>
+            </VForm>
+        </LayoutBaseDePagina>
+    );
+};
--- src/pages/bancos/ListagemDeBancos.tsx
+++ src/pages/bancos/ListagemDeBancos.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
@@ -9,73 +10,73 @@ import { useDebounce } from "../../shared/hooks";
 import { LayoutBaseDePagina } from "../../shared/layouts"
 
 export const ListagemDeBanco: React.FC = () => {
-    const navigate = useNavigate();
-//tempo de busca ao digitar
-    const { debounce } = useDebounce(600, false);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const [totalCount, setTotalCount] = useState(0);
-    const [rows, setRows] = useState<IListagemBanco[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+  //tempo de busca ao digitar
+  const { debounce } = useDebounce(600, false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [totalCount, setTotalCount] = useState(0);
+  const [rows, setRows] = useState<IListagemBanco[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    //permite edicao do campo de input
-    const busca = useMemo(() => {
-        return searchParams.get('busca') || '';
-    }, [searchParams]);
+  //permite edicao do campo de input
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
 
-    const pagina = useMemo(() => {
-        return Number(searchParams.get("pagina") || "1");
-    }, [searchParams]);
-    const handleDelete = (id: number) => {
-        if(window.confirm('Mano, tem certeza que quer apagar o registro '+id+' ?')){
-           
-          BancosService.deleteById(id)
-          .then(result =>{
-            if(result instanceof Error){
-              alert(result.message);
-            } else{
-              setRows(oldRows => [
-                ...oldRows.filter(oldRow => oldRow.id !== id),
-              ]);
-              alert('Registro apagado com sucesso!');
-            }
-          })
-        };
+  const pagina = useMemo(() => {
+    return Number(searchParams.get("pagina") || "1");
+  }, [searchParams]);
+  const handleDelete = (id: number) => {
+    if (window.confirm('Mano, tem certeza que quer apagar o registro ' + id + ' ?')) {
+
+      BancosService.deleteById(id)
+        .then(result => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            setRows(oldRows => [
+              ...oldRows.filter(oldRow => oldRow.id !== id),
+            ]);
+            alert('Registro apagado com sucesso!');
+          }
+        })
     };
+  };
 
-    useEffect(() => {
-        setIsLoading(true);
+  useEffect(() => {
+    setIsLoading(true);
 
-        debounce(() => {
-            BancosService.get(pagina, busca)
-            .then((result) => {
-                setIsLoading(false);
+    debounce(() => {
+      BancosService.get(pagina, busca)
+        .then((result) => {
+          setIsLoading(false);
 
-                if (result instanceof Error) {
-                    alert(result.message);
-                    return;
-                } else {
-                    setTotalCount(result.totalCount);
+          if (result instanceof Error) {
+            alert(result.message);
+            return;
+          } else {
+            setTotalCount(result.totalCount);
 
-                    setRows(result.data);
-                }
-                console.log(result);
-            });
+            setRows(result.data);
+          }
+          console.log(result);
         });
-        }, [busca, debounce, pagina]);
+    });
+  }, [busca, debounce, pagina]);
 
-    return (
-        <LayoutBaseDePagina titulo='Listagem de bancos'
-            barraDeFerramentas={
-                <FerramentasDaListagem
-                    mostrarInputBusca
-                    textoBotaoNovo="Nova"
-                    aoClicarEmNovo={() => navigate('/banco/nova')}
-                    textoDaBusca={searchParams.get('busca') ?? ''}
-                    aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
-                />
-            }
-        >
- <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: "auto" }}>
+  return (
+    <LayoutBaseDePagina titulo='Listagem de bancos'
+      barraDeFerramentas={
+        <FerramentasDaListagem
+          mostrarInputBusca
+          textoBotaoNovo="Nova"
+          aoClicarEmNovo={() => navigate('/bancos/nova')}
+          textoDaBusca={searchParams.get('busca') ?? ''}
+          aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+        />
+      }
+    >
+      <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: "auto" }}>
         <Table>
           <TableHead>
             <TableRow>
@@ -92,13 +93,13 @@ export const ListagemDeBanco: React.FC = () => {
                   <IconButton size="small" onClick={() => handleDelete(row.id)}>
                     <Icon>delete</Icon>
                   </IconButton>
-                  <IconButton size="small" onClick={() => navigate(`/banco/${row.id}`)} >
+                  <IconButton size="small" onClick={() => navigate(`/bancos/${row.id}`)} >
                     <Icon>edit</Icon>
                   </IconButton>
                 </TableCell>
                 <TableCell>{row.nome}</TableCell>
-                <TableCell>{row.ativo == '1' ? "Sim" : "Não" }</TableCell>
-                
+                <TableCell>{row.ativo == '1' ? "Sim" : "Não"}</TableCell>
+
               </TableRow>
             ))}
           </TableBody>
@@ -106,46 +107,29 @@ export const ListagemDeBanco: React.FC = () => {
             <caption>{Environment.LISTAGEM_VAZIA}</caption>
           )}
           <TableFooter>
-            
 
-              {isLoading && (
-                <TableRow>
+            {isLoading && (
+              <TableRow>
                 <TableCell colSpan={3}>
                   <LinearProgress variant="indeterminate" />
                 </TableCell>
-                </TableRow>
-              )}
+              </TableRow>
+            )}
 
-              
-              {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS ) && (
-                <TableRow>
+            {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS) && (
+              <TableRow>
                 <TableCell colSpan={3}>
-                  <Pagination 
+                  <Pagination
                     page={pagina}
-                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)} 
-                    onChange={(_, newPage) => setSearchParams({busca, pagina: newPage.toString() },{replace: true})}
+                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                    onChange={(_, newPage) => setSearchParams({ busca, pagina: newPage.toString() }, { replace: true })}
                   />
                 </TableCell>
-                </TableRow>
-              )}
-
-            
+              </TableRow>
+            )}
           </TableFooter>
         </Table>
       </TableContainer>
-
-
-
-
-
-        </LayoutBaseDePagina>
-
-
-
-
-
-
-
-
-    )
+    </LayoutBaseDePagina>
+  )
 }
